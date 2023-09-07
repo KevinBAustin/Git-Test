08f3cae4077164fd56befe09f77dd44d82724669
KevinBAustin <kevbaust@ut.utm.edu>
2023-09-07 14:51:29 -0500

    Added Features to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,29 @@ using namespace std;
 
 int sum(int n);
 
+int product(int n);
+
 int main(){
   int n;
   cout<<"Enter a number"<<endl;
   cin>>n;
 
   cout<<sum(n)<<endl;
+  cout<<product(n)<<endl;
 }
 
 int sum(int n){
   int tot=0;
   for(int i=1;i<=n;i++){
     tot+=i;
+  }
+  return tot;
+}
+
+int product(int n){
+  int tot=1;
+  for(int i=1;i<=n;i++){
+    tot=tot*i;
   }
   return tot;
 }
